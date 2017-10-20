ae57ae1add26503bedb6f0fe00ec9225b166336b
Kara Maedje <karamaedje26@hotmail.com>
2017-10-20 16:40:34 -0400

    Add files via upload

--- include/powerfunction.h
+++ include/powerfunction.h
@@ -1,1 +1,12 @@
-double powerfn(double x, int n);
+#include <stdio.h>
+
+double powerfn(double x,int n){
+	int i;
+	double result = 1;
+
+	for(i=1; i<=n; i++){
+		*result = x;
+	}
+	return result;
+}
+
