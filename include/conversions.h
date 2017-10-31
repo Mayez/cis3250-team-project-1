50bd76e636e265a74cf9878510cbacba43e6b303
JasonEllul <31999426+JasonEllul@users.noreply.github.com>
2017-10-31 16:14:52 -0400

    Add files via upload

---
+++ include/conversions.h
@@ -0,0 +1,7 @@
+// Description: Function void time() calculates time for
+// the legacy calculator  
+void time();
+
+// Description: Function void temp() calculates the temperature
+// for the legacy calculator
+void temp();
